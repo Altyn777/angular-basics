f1cbe8deb746756ea630456306b3553db825b2cd
Altyn <darzeeia@gmail.com>
2020-05-07 10:44:56 +0300

    post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {PostComponent} from "./post/post.component";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule
